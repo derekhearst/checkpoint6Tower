c22795e996ee7001b46059f69241dc27296794f9
Derek Hearst <derekallenhearst@gmail.com>
2023-01-24 14:54:17 -0700

    updated config

--- checkpoint6Tower.client/src/pages/HomePage.vue
+++ checkpoint6Tower.client/src/pages/HomePage.vue
@@ -54,7 +54,7 @@ watchEffect(() => {
   <div class="homePage">
     <div class="heroContainer">
       <img class="heroImg"
-        src="https://s3-alpha-sig.figma.com/img/cd1b/eb35/3224c652f40c0754047c232b09e71cc3?Expires=1672012800&Signature=RW1nLQQV8mXj05s9VlVTLiUq3suFwCVdXGSvgqCseygTB5HSQpBLsn8~rVTExXTnwq2VlhL5C7QVuGPD6fsQsP029P0524QX-tyTB~Nb2n335I0ITFjnW-ziH3D6N0Rb-bRK6FGjR7E~-VDM93YxsoQSXWM7Asbt-FBYhAcfYMWggffjcMa5pSPHHmvf4sI7msLEyLhCZBjCn3xvk~TJr4274BvOiqjoOWYg0e9e3l1mDIkFmTV4~~llzxH~999X6Yqv2jmRXHWcLtDJogvPNS8AL6vHwmQTatIXzN7HSpTlm6Epj7SorOu3SPAPqe3sqc7H~N1rcvL2OjNlHVJwYg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+        src="https://images.unsplash.com/photo-1574267432553-4b4628081c31?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2luZW1hJTIwc2VhdHN8ZW58MHx8MHx8&w=1000&q=80"
         alt="A picture of a chair in a theater" />
 
 
